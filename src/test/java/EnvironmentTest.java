b4a28ab45876648c6d4e32fde17be88a6fb29af0
Andrew Melton <ramielrowe@gmail.com>
2013-01-13 23:40:10 -0500

    Adding a quick test to test ci environment

---
+++ src/test/java/EnvironmentTest.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+public class EnvironmentTest{
+
+    @Test
+    public void ShouldAlwaysBeTrue() {
+        assertTrue(1 == 1);
+    }
+
+}
